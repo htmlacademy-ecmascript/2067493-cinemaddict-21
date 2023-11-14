61e59c1116ad2397168395da70190a43a03d723e
Nik NSM <nsm020598@gmail.com>
2023-11-15 00:07:37 +1000

    Реализуем сортировку

--- src/presenter/presenter.js
+++ src/presenter/presenter.js
@@ -8,31 +8,35 @@ import MovieCardPresenter from './movie-card-presenter.js';
 import Empty from '../view/empty.js';
 import { updateItem } from '../utils.js';
 import { render, remove } from '../framework/render.js';
+import { SORT_TYPE } from '../const.js';
 
 const MOVIES_COUNT_PER_STEP = 5;
 
 export default class Presenter {
   #infoUser = new InfoUser();
-  #listSort = new ListSort();
   #containerMovies = new ContainerMovies();
   #moviesList = new MoviesList();
   #numberOfFilms = new NumberOfFilms();
   #empty = new Empty();
   #cardsMoviesPresentrs = new Map();
+  #listSort = null;
   #bodyContainer = null;
   #showMoreButton = null;
   #containerInfoUser = null;
   #contentContainer = null;
   #containerNumberOfFilms = null;
+  #sortMoviesDefault = [];
   #movies = [];
   #comments = [];
   #renderedMoviesCount = MOVIES_COUNT_PER_STEP;
+  #currentSortType = SORT_TYPE.DEFAULT;
 
   constructor({ containerInfoUser, contentContainer, containerNumberOfFilms, moviesModel, comments, body }) {
     this.#containerInfoUser = containerInfoUser;
     this.#contentContainer = contentContainer;
     this.#containerNumberOfFilms = containerNumberOfFilms;
     this.#movies = [...moviesModel];
+    this.#sortMoviesDefault = [...this.#movies];
     this.#comments = comments;
     this.#bodyContainer = body;
   }
@@ -42,8 +46,6 @@ export default class Presenter {
     this.#renderListSort();
     this.#renderContainerMovies();
     this.#renderListMovies();
-    this.#renderCards({ container: this.#moviesList.element });
-    this.#renderShowMoreButton();
     this.#renderNumberOfFilms();
   }
 
@@ -51,7 +53,32 @@ export default class Presenter {
     render(this.#infoUser, this.#containerInfoUser);
   }
 
+  #handleChangeSort = (sortType) => {
+    if (sortType === this.#currentSortType) {
+      return;
+    }
+
+
+    switch (sortType) {
+      case SORT_TYPE.DATE:
+        this.#movies.sort((a, b) => b.filmInfo.releas.date - a.filmInfo.releas.date);
+        break;
+      case SORT_TYPE.RETING:
+        this.#movies.sort((a, b) => b.filmInfo.totalRating - a.filmInfo.totalRating);
+        break;
+      default:
+        this.#movies = [...this.#sortMoviesDefault];
+    }
+
+    this.#currentSortType = sortType;
+    this.#clearMoviesCard();
+    this.#renderListMovies();
+  };
+
   #renderListSort() {
+    this.#listSort = new ListSort({
+      onChangeSort: this.#handleChangeSort
+    });
     render(this.#listSort, this.#contentContainer);
   }
 
@@ -59,27 +86,29 @@ export default class Presenter {
     render(this.#containerMovies, this.#contentContainer);
   }
 
-  #renderEmpty () {
+  #renderEmpty() {
     render(this.#empty, this.#containerMovies.element);
   }
 
   #renderListMovies() {
-    if(this.#movies.length === 0){
+    if (this.#movies.length === 0) {
       this.#renderEmpty();
       return;
     }
     render(this.#moviesList, this.#containerMovies.element);
+    this.#renderCards();
+    this.#renderShowMoreButton();
   }
 
-  #renderCards = ({ container }) => {
+  #renderCards = () => {
     this.#movies
       .slice(0, Math.min(this.#movies.length, this.#renderedMoviesCount))
-      .forEach((movie) => this.#renderCardMovie({ containerCards: container, movie }));
+      .forEach((movie) => this.#renderCardMovie(movie));
   };
 
-  #renderCardMovie({ containerCards, movie }) {
+  #renderCardMovie(movie) {
     const movieCard = new MovieCardPresenter({
-      containerCards,
+      containerCards: this.#moviesList.element,
       comments: this.#comments,
       bodyContainer: this.#bodyContainer,
       onDateChange: this.#handlerChangeMovies,
@@ -94,7 +123,7 @@ export default class Presenter {
   };
 
   #clearMoviesCard() {
-    this.#cardsMoviesPresentrs.forEach((presenter) => presenter.destroy);
+    this.#cardsMoviesPresentrs.forEach((presenter) => presenter.destroy());
     this.#cardsMoviesPresentrs.clear();
     this.#renderedMoviesCount = MOVIES_COUNT_PER_STEP;
     remove(this.#showMoreButton);
@@ -113,7 +142,7 @@ export default class Presenter {
   #handlerClickShowMoreButton = () => {
     this.#movies
       .slice(this.#renderedMoviesCount, this.#renderedMoviesCount + MOVIES_COUNT_PER_STEP)
-      .forEach((movie) => this.#renderCardMovie({ containerCards: this.#moviesList.element, movie }));
+      .forEach((movie) => this.#renderCardMovie(movie));
 
     this.#renderedMoviesCount += MOVIES_COUNT_PER_STEP;
 
@@ -128,6 +157,7 @@ export default class Presenter {
 
   #handlerChangeMovies = (updateMovie) => {
     this.#movies = updateItem(this.#movies, updateMovie);
+    this.#sortMoviesDefault = updateItem(this.#sortMoviesDefault, updateMovie);
     this.#cardsMoviesPresentrs.get(updateMovie.id).init(updateMovie);
   };
 
