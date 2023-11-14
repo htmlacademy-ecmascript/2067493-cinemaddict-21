f4a9326a66fb87662360d830298b51aa4c699d83
Nik NSM <nsm020598@gmail.com>
2023-11-14 23:22:27 +1000

    Добавит дата атрибут элементам сортировки

--- src/view/view-template/list-sort-template.js
+++ src/view/view-template/list-sort-template.js
@@ -1,9 +1,11 @@
+import { SORT_TYPE } from '../../const.js';
+
 function createListSortTemplate () {
   return `
   <ul class="sort">
-    <li><a href="#" class="sort__button sort__button--active">Sort by default</a></li>
-    <li><a href="#" class="sort__button">Sort by date</a></li>
-    <li><a href="#" class="sort__button">Sort by rating</a></li>
+    <li><a href="#" class="sort__button sort__button--active" data-sort-type=${SORT_TYPE.DEFAULT}>Sort by default</a></li>
+    <li><a href="#" class="sort__button" data-sort-type=${SORT_TYPE.DATE}>Sort by date</a></li>
+    <li><a href="#" class="sort__button" data-sort-type=${SORT_TYPE.RETING}>Sort by rating</a></li>
   </ul>
   `;
 }
